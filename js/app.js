8349e3f9d4d2ef4bb054030691b3dcb763f87c1b
Kurtius <daniel@offhand.io>
2020-12-29 16:53:53 +0100

    Initial "Soon…" & basics

---
+++ js/app.js
@@ -0,0 +1,1 @@
+function ready(e){"loading"!=document.readyState?e():document.addEventListener("DOMContentLoaded",e)}ready(function(){console.log("ready"),script_lazy=document.createElement("script"),script_lazy.async=!0,script_lazy.src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js",document.body.appendChild(script_lazy)});
